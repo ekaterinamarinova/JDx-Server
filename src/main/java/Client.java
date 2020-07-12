1cd56338c3466aea41a6d5b631e43061a18780ff
ekaterinamarinova <katherineeyall@gmail.com>
2020-07-13 01:50:02 +0300

    Renamed Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.net.Socket;
 
-public class Server {
+public class Client {
     public static void main(String[] args) throws IOException {
         Socket s = new Socket("localhost", 8080);
         s.getOutputStream().write(new byte[]{101, 30, 40});
